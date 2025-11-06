40fae7809ef5bfa0e85a3e0a87a1629a46fc9b09
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2025-11-06 15:27:49 -0500

    Adding what I've worked on so far, setting up publish workflow to website

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,144 @@
-<h1><?= $page->title() ?></h1>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <?= css([
+      'assets/styles/main-min.css',
+    ]) ?>
+  </head>
+  <body class="wrapper">
+    <script>
+        var app = document.getElementsByTagName("BODY")[0];
+        if (localStorage.lightMode == "dark") {
+            app.setAttribute("data-color-mode", "dark");
+        }
+    </script>
+    <header>
+      <section class="masthead">
+        <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg" title="Brandmark">
+          <description>Horizontally & vertically flipped, circularly encased ampersand figureground.</description>
+          <circle class="circle" cx="32" cy="32" r="32" fill="var(--theme-darkest)"/>
+          <path class="shape" d="M21.318 14.154C23.3287 14.154 25.2813 14.6373 27.176 15.604C29.032 16.5707 30.7913 17.866 32.454 19.49C34.1167 21.1527 35.6247 22.9893 36.978 25C38.3313 27.0493 39.5107 29.1567 40.516 31.322C41.5213 33.526 42.2947 35.672 42.836 37.76C43.3773 39.848 43.648 41.7427 43.648 43.444C43.648 45.3387 43.2807 47.0787 42.546 48.664C41.7727 50.288 40.6127 51.5833 39.066 52.55C37.4807 53.5167 35.47 54 33.034 54C30.83 54 28.974 53.594 27.466 52.782C25.958 52.0087 24.8173 50.9647 24.044 49.65C23.232 48.3353 22.826 46.866 22.826 45.242C22.826 43.9273 23.1353 42.6513 23.754 41.414C24.334 40.2153 25.3393 39.1133 26.77 38.108C28.2007 37.1027 30.134 36.2713 32.57 35.614L33.034 36.6C31.4873 37.8373 30.3853 39.0553 29.728 40.254C29.032 41.4913 28.684 42.7673 28.684 44.082C28.684 45.4353 29.0513 46.576 29.786 47.504C30.482 48.432 31.5453 48.896 32.976 48.896C34.02 48.896 34.8707 48.6447 35.528 48.142C36.1853 47.678 36.6687 47.0207 36.978 46.17C37.2487 45.358 37.384 44.43 37.384 43.386C37.384 42.1487 37.1713 40.6987 36.746 39.036C36.3207 37.3733 35.7407 35.6527 35.006 33.874C34.2327 32.0953 33.3433 30.3553 32.338 28.654C31.3327 26.9527 30.2307 25.406 29.032 24.014C27.8333 22.6607 26.596 21.578 25.32 20.766C24.0053 19.954 22.6907 19.548 21.376 19.548C20.2933 19.548 19.4427 19.8187 18.824 20.36C18.1667 20.94 17.838 21.6747 17.838 22.564C17.838 23.5307 18.2053 24.478 18.94 25.406C19.636 26.334 20.6607 27.1653 22.014 27.9C23.3287 28.6733 24.9527 29.2533 26.886 29.64V35.15H13.488V30.278L24.044 31.728V30.568C20.0613 29.8333 17.1227 28.6927 15.228 27.146C13.3333 25.5993 12.386 23.6853 12.386 21.404C12.386 20.0507 12.676 18.8907 13.256 17.924C13.836 16.996 14.59 16.2613 15.518 15.72C16.446 15.1787 17.432 14.792 18.476 14.56C19.4813 14.2893 20.4287 14.154 21.318 14.154ZM41.966 14.154C43.938 14.154 45.6973 14.5213 47.244 15.256C48.752 16.0293 49.9313 17.0927 50.782 18.446C51.594 19.838 52 21.4427 52 23.26C52 24.884 51.6713 26.3147 51.014 27.552C50.318 28.7893 49.4093 29.814 48.288 30.626C47.128 31.4767 45.8327 32.1533 44.402 32.656C42.9713 33.1973 41.5213 33.5647 40.052 33.758L39.762 32.25C41.8113 31.322 43.3773 30.2007 44.46 28.886C45.504 27.61 46.026 26.218 46.026 24.71C46.026 23.5887 45.7747 22.564 45.272 21.636C44.7693 20.7467 44.0347 20.0313 43.068 19.49C42.0627 18.9873 40.8447 18.736 39.414 18.736C38.0993 18.736 36.92 19.0067 35.876 19.548C34.832 20.0893 33.846 20.8047 32.918 21.694L31.468 19.49H32.86C33.44 18.4073 34.1553 17.46 35.006 16.648C35.8567 15.8747 36.862 15.2753 38.022 14.85C39.1433 14.386 40.458 14.154 41.966 14.154Z" fill="var(--theme-lightest)"/>
+        </svg>
+        <h1>Poet & Scribe</h1>
+      </section>
+      <p class="lede">Helping organizations create digital products that work for everyone through accessible design systems and thoughtful strategy.</p>
+    </header>
+    <main>
+      <article>
+        <h2>About us</h2>
+        <p>Poet & Scribe was founded in 2022 by product development director and principal designer, Jonathan Stephens.</p>
+        <p>With P&S, <em>he/they</em> partners with trusted organizations and freelancers to provide a rounded and reliable service.</p>
+
+        <h3>What we do</h2>
+        <p class="">We specialize in product strategy and design systems for scale-ups, non-profits, and global enterprises.</p>
+
+        <h3>Design systems</h3>
+        <p>If you're looking to fix disjointed user interfaces, improve internal efficiency, or build a resilient product—we're available to help with short-to-mid-term design system projects.</p>
+        <p>We create <a href="https://designsystem.university/articles/what-is-a-design-system" title="A post by Dan Mall of Design Systems University defining six different types of design systems">all types of design systems</a> that're accessible, clear, and deliver compounding value to users and your bottom line.</p>
+
+        <h3>Product strategy</h3>
+        <p>We’ve worked in product development throughout our careers—from a decade of scaling global enterprises to consulting with agencies, start-ups, and governments.</p>
+        <p>Each client is unique, in their own season of growth & scale. We focus on understanding <em>your context</em> and co-creating paths forward together.</p>
+        <p>If you’re looking for direction, optimizing features, or scaling your product, we offer consultancy and coaching sessions to support you.</p>
+
+        <h3>Can’t find what you need?</h3>
+        <p>We’re happy to chat if you need help with something we haven’t mentioned.</p>
+        <p>If we can’t help, we’ll work to connect you with someone who can—we’ve met a lot of people throughout our careers.</p>
+
+        <h2>Who we’ve worked with</h2>
+        <p>We’ve built our careers working on digital products for some local & global organizations and teams, including:</p>
+        <ul>
+          <li>Booking.com</li>
+          <li>United States' Space Force</li>
+          <li>Leantime.io</li>
+          <li>Strategi Consulting</li>
+          <li>Trailmarks HQ</li>
+          <li>Smashing Boxes</li>
+        </ul>
+
+        <h2>Our ethos</h2>
+        <p>Poet & Scribe isn't just our name, it defines how we operate.</p>
+
+        <p>Poets take what’s hard to articulate and find language for it. They're sensemakers of patterns and meaning. Scribes preserve knowledge, ensuring continuity. They build context, shaping how communities grow & scale beyond any one human's capability.</p>
+
+        <p>They create shared understanding.</p>
+
+        <p>In product development, it's much of the same. Constantly translating between perspectives: user research &#8594; strategy; strategy → systems; systems → practices.</p>
+
+        <p>We help organizations get better at this. We work to understand where you’ve been, what you’ve learned, and what you’re actually trying to build.</p>
+
+
+        <h2>Guiding principles</h2>
+        <p>We're not disruptors, trend-setters, revolutionaries, troublemakers, or thing-breakers.</p>
+
+        <ul>
+          <li>
+            <strong>Coalition, not competition.</strong>
+            <p>We believe in collective power over individual heroics, building with others rather than against them.</p>
+          </li>
+          <li>
+            <strong>Spectrums, not binaries.</strong>
+            <p>We recognize that most decisions exist on a continuum, embracing nuance and context over rigid absolutes.</p>
+          </li>
+          <li>
+            <strong>Curiosity, not assumptions.</strong>
+            <p>We ask questions and listen deeply, approaching each project with genuine interest rather than predetermined solutions.</p>
+          </li>
+          <li>
+            <strong>Sustainable, not extractive.</strong>
+            <p>We build for longevity and regeneration, creating systems that endure and improve over time rather than depleting resources for short-term gains.</p>
+          </li>
+          <li>
+            <strong>Iteration, not perfection.</strong>
+            <p>We embrace learning through doing, refining our work through cycles of testing and feedback rather than seeking flawless first attempts.</p>
+          </li>
+        </ul>
+
+        <h2>Let’s talk</h2>
+        <p>If you’re interested in the support we offer, we’d love to chat</p>
+
+        <p>Email us at hello@poetandscribe.com.</p>
+      </article>
+    </main>
+    <footer>
+      <button
+    		class="light-mode-button" aria-label="Toggle Light Mode" onclick="toggle_light_mode()">
+	    	<span></span>
+	    	<span></span>
+	    </button>
+      <p class="wrapper">Thanks for stopping by</p>
+      <ul class="palette">
+        <li>lightest</li>
+        <li>lighter</li>
+        <li>light</li>
+        <li>mid</li>
+        <li>dark</li>
+        <li>darker</li>
+        <li>darkest</li>
+      </ul>
+    </footer>
+    <script>
+        function toggle_light_mode() {
+            var app = document.getElementsByTagName("BODY")[0];
+            if (localStorage.lightMode == "dark") {
+                localStorage.lightMode = "light";
+                app.setAttribute("data-color-mode", "light");
+            } else {
+                localStorage.lightMode = "dark";
+                app.setAttribute("data-color-mode", "dark");
+            }
+        }
+
+		window.addEventListener("storage", function () {
+			if (localStorage.lightMode == "dark") {
+				app.setAttribute("data-color-mode", "dark");
+			} else {
+				app.setAttribute("data-color-mode", "light");
+			}
+		}, false);
+
+    </script>
+  </body>
+</html>
